f1d4bb64b7cc81f7e41811393a93cc94a295c8f5
Swapnoshis Sikder <swapno.duet@gmail.com>
2025-01-27 21:55:58 +0600

    create a live chatbox

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,4 +15,32 @@ toggle_btn.onclick = (e)=>{
         icon.classList.add("fa-bars");
     }
 };
+
+
+// Live Chat JS
+let chat_icon = document.querySelector(".my_chat_wrapper .chat_icon");
+let chat_box = document.querySelector(".my_chat_wrapper .chat_box");
+chat_icon.addEventListener("click",()=>{
+    chat_box.classList.toggle("active");
+});
+
+// Message Send
+let send_btn = document.querySelector("#send_button");
+
+
+send_btn.addEventListener('click',()=>{
+    let message_input = document.querySelector("#message_input");
+    let message_wrapper = document.querySelector(".messages");
+    let message = message_input.value;
+    let message_body = `<div class="sender d-flex align-items-center mb-3 justify-content-left">
+                            <div class="author_img rounded-circle">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <div class="message">
+                                <p>${message}</p>
+                            </div>
+                        </div>`;
+    message_wrapper.innerHTML +=message_body;
+    message_input.value = "";
+});
 });
